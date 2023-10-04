19687bbd641dbd4fc071d6355afa8e7094ce9691
Aatif Syed <aatifsyedyp@gmail.com>
2023-10-04 03:09:59 +0100

    refactor: always have source code

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ use miette::{
 
 #[derive(Debug, Clone)]
 pub struct Error {
-    source_code: Option<MaybeNamedSource<Arc<str>>>,
+    source_code: MaybeNamedSource<Arc<str>>,
     syn_error: syn::Error,
 }
 
@@ -27,14 +27,14 @@ impl Error {
     /// This is because the [`syn::Error::span`] may be out-of-bounds.
     pub fn new(syn_error: syn::Error, source_code: impl Into<Arc<str>>) -> Self {
         Self {
-            source_code: Some(MaybeNamedSource {
+            source_code: MaybeNamedSource {
                 file_name: None,
                 source_code: source_code.into(),
-            }),
+            },
             syn_error,
         }
     }
-    /// Create an error with a filename for the source code
+    /// Create an error with a filename for the source code.
     ///
     /// ```text
     ///   ╭─[/path/to/file:1:1]
@@ -54,21 +54,24 @@ impl Error {
         file_name: impl fmt::Display,
     ) -> Self {
         Self {
-            source_code: Some(MaybeNamedSource {
+            source_code: MaybeNamedSource {
                 file_name: Some(file_name.to_string()),
                 source_code: source_code.into(),
-            }),
+            },
             syn_error,
         }
     }
-}
-
-impl From<syn::Error> for Error {
-    fn from(syn_error: syn::Error) -> Self {
-        Self {
-            source_code: None,
-            syn_error,
-        }
+    /// Get a reference to the source code.
+    pub fn source_code(&self) -> &Arc<str> {
+        &self.source_code.source_code
+    }
+    /// Get an exclusive reference to the [`syn::Error`], for e.g calling [`syn::Error::combine`]
+    pub fn get_mut(&mut self) -> &mut syn::Error {
+        &mut self.syn_error
+    }
+    /// Convert this back to the original [`syn::Error`], discarding the source code.
+    pub fn into_inner(self) -> syn::Error {
+        self.into()
     }
 }
 
@@ -80,35 +83,32 @@ impl From<Error> for syn::Error {
 
 impl Diagnostic for Error {
     fn source_code(&self) -> Option<&dyn SourceCode> {
-        self.source_code.as_ref().map(|it| it as &dyn SourceCode)
+        Some(&self.source_code as &dyn SourceCode)
     }
 
     fn labels(&self) -> Option<Box<dyn Iterator<Item = miette::LabeledSpan> + '_>> {
-        match &self.source_code {
-            Some(MaybeNamedSource {
-                file_name: _,
-                source_code,
-            }) => Some(Box::new((&self.syn_error).into_iter().map(
-                move |syn_error| {
-                    let span = syn_error.span();
-                    let span_start = span.start();
-                    let span_end = span.end();
-                    let start_offset = SourceOffset::from_location(
-                        source_code,
-                        span_start.line,
-                        span_start.column,
-                    );
-                    let end_offset =
-                        SourceOffset::from_location(source_code, span_end.line, span_end.column);
-                    let length = SourceOffset::from(end_offset.offset() - start_offset.offset());
-                    LabeledSpan::new_with_span(
-                        Some(syn_error.to_string()),
-                        SourceSpan::new(start_offset, length),
-                    )
-                },
-            ))),
-            None => None,
-        }
+        Some(Box::new((&self.syn_error).into_iter().map(
+            move |syn_error| {
+                let span = syn_error.span();
+                let span_start = span.start();
+                let span_end = span.end();
+                let start_offset = SourceOffset::from_location(
+                    &self.source_code.source_code,
+                    span_start.line,
+                    span_start.column,
+                );
+                let end_offset = SourceOffset::from_location(
+                    &self.source_code.source_code,
+                    span_end.line,
+                    span_end.column,
+                );
+                let length = SourceOffset::from(end_offset.offset() - start_offset.offset());
+                LabeledSpan::new_with_span(
+                    Some(syn_error.to_string()),
+                    SourceSpan::new(start_offset, length),
+                )
+            },
+        )))
     }
 }
 
@@ -168,14 +168,12 @@ mod tests {
     use super::*;
 
     enum Behaviour {
-        Bare,
         IncludeSource,
         IncludeFilename,
     }
 
     #[test]
     fn basic_parse() {
-        insta::assert_snapshot!(test_parse::<DeriveInput>("struct Foo", Behaviour::Bare));
         insta::assert_snapshot!(test_parse::<DeriveInput>(
             "struct Foo",
             Behaviour::IncludeSource
@@ -188,10 +186,6 @@ mod tests {
 
     #[test]
     fn call_site() {
-        insta::assert_snapshot!(render(Error::from(syn::Error::new(
-            Span::call_site(),
-            "the whole thing is fucked"
-        ))));
         insta::assert_snapshot!(render(Error::new(
             syn::Error::new(Span::call_site(), "the whole thing is fucked"),
             "this is the source code"
@@ -207,10 +201,6 @@ mod tests {
         insta::assert_snapshot!(test_parse::<UniqueIdents>(
             "hello\nworld\nhello",
             Behaviour::IncludeSource
-        ));
-        insta::assert_snapshot!(test_parse::<UniqueIdents>(
-            "hello\nworld\nhello",
-            Behaviour::Bare
         ));
         insta::assert_snapshot!(test_parse::<UniqueIdents>(
             "hello\nworld\nhello",
@@ -243,7 +233,6 @@ mod tests {
             panic!("parsing succeeded where it was expected to fail")
         };
         let error = match behaviour {
-            Behaviour::Bare => Error::from(error),
             Behaviour::IncludeSource => Error::new(error, source_code),
             Behaviour::IncludeFilename => Error::new_named(error, source_code, "/path/to/file"),
         };
